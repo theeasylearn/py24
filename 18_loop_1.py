a6e5aa0137f5f685571f5664041bc7632fea5873
theeasylearn <theeasylearn@gmail.com>
2025-05-21 18:35:41 +0530

    loop

---
+++ 18_loop_1.py
@@ -0,0 +1,25 @@
+'''
+    write a program to print following pattern 
+    2   4   6   8   10  12  .....   100
+    steps 
+    ----------------------------------
+    1)  create variable number 
+    2)  store 2 into number
+    3)  print number 
+    4)  number(4) = number + 2 
+    5)  print number
+    6)  number(6) = number + 2 
+    7)  print number
+    
+2 4 6 
+'''
+number = 2 #2
+
+while number<=100: #False
+    print(number,end= ' ') #8
+    number = number + 2 #102
+
+print() #new line
+print("good bye")
+
+
