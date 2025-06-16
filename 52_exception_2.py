837c691f4aad835eb1a311ca903191835f2034da
theeasylearn <theeasylearn@gmail.com>
2025-06-16 18:50:48 +0530

    exception

---
+++ 52_exception_2.py
@@ -0,0 +1,17 @@
+#write a program to findout qube of given positive number with exception handling mechanism 
+import math
+# infinite loop 
+while True:
+    try:
+        number = int(input("Enter positive number to findout qube")) #this line can generate exception (run time error due to invalid input)
+        if number<0:
+            print('it is negative number, so let us convert it into positive number')
+        number = math.fabs(number)
+        qube = number * number * number 
+        print(f"qube = {qube}")
+        break #will stop loop
+    except ValueError:
+        #except block will run only in case of ValueError exception
+        print("it is not valid input")
+print('program have finished.....')
+
---
+++ 53_exception.py
@@ -0,0 +1,2 @@
+# example of simple user defined exception 
+# write a program to accept amount user want's do withdraw from his bank account. if remaining amount is less then 5000 Rs then do not allowed user to withdraw money. allow user to do 3 attempt 
