678b2184ca22c874f2b85f246e7101101685082f
theeasylearn <theeasylearn@gmail.com>
2025-07-21 18:46:16 +0530

    database

--- database.py
+++ database.py
@@ -15,15 +15,12 @@ try:
 except errors.InterfaceError as err:
     print(f"invalid port no: {err}")
     # This handles errors like 'Can't connect to MySQL server on 'localhost:3305' (10061)'
-    
 except errors.ProgrammingError as err:
     print(f"invalid database name (database does not exists) {err}")
     # This handles errors like 'Unknown database 'py2'' or 'Access denied'
-
 except errors.NotSupportedError as err:
     print(f"invalid password for user: {err}")
     # This handles errors like 'Authentication plugin 'caching_sha2_password' is not supported'
-    
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
     # This catches any other exceptions not specifically handled above
