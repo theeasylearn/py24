3ace55ba6b842731185d71a132ddcee3765ce86b
theeasylearn <theeasylearn@gmail.com>
2025-07-23 18:39:13 +0530

    select.py

--- 83_select.py
+++ 83_select.py
@@ -4,7 +4,7 @@
 import dbconnction as con #here con is nick name for database
 
 #create cursor type object
-mycursor = con.database.cursor()
+mycursor = con.database.cursor(dictionary=True)
 
 #create sql variable(string) to store sql command(insert/update/delete/select any sql command in this case select)
 sql = "select * from category order by id desc"
@@ -19,4 +19,14 @@ mycursor.execute(sql)
 
 #fetch all row 
 table = mycursor.fetchall()
-print(table)
+# print(table)
+print("_"*115)
+print(f"{'id':6}{'name':32}{'detail':48}{'status':10}{'created_at':12}")
+print("_"*115)
+for row in table:
+    if row['status'] == 0:
+        row['status'] = 'Not Live'
+    else: 
+        row['status'] = 'Live'
+    created_at = row['created_at'].strftime('%d-%m-%Y %H:%M:%S')
+    print(f"{row['id']:2}{'':4}{row['name']:32}{row['detail']:48}{row['status']:10}{created_at:18}")
